9a27135b9dac34cd333f2bb962115deedf757d51
Om Singh Mehta <ommehta912@gmail.com>
2025-06-04 22:00:32 +0530

    routes setted

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/student/Home"
+import CoursesList from './pages/student/CoursesList.jsx'
+import CourseDetails from './pages/student/CourseDetails';
+import MyEnrollments from './pages/student/MyEnrollments'
+import Player from './pages/student/Player'
+import Loading from './components/student/Loading'
+import Educator from './pages/educator/Educator';
+import DashBoard from './pages/educator/DashBoard';
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import StudentEnrolled from './pages/educator/StudentsEntrolled'
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/course-list' element={<CoursesList/>}/>
+        <Route path='/course/:id' element={<CourseDetails/>}/>
+        <Route path='/course-list/:input' element={<CoursesList/>}/>
+        <Route path='/my-enrollments' element={<MyEnrollments/>}/>
+        <Route path='/player/:courseId' element={<Player/>}/>
+        <Route path='/loading/:path' element={<Loading/>}/>
+
+        {/* educator */}
+        <Route path='/educator' element={<Educator/>}>
+        <Route path='educator' element={<DashBoard/>}></Route>
+        <Route path='add-course' element={<AddCourse/>}></Route>
+        <Route path='my-course' element={<MyCourses/>}></Route>
+        <Route path='student-enrolled' element={<StudentEnrolled/>}></Route>
+
+
+        </Route>
+      </Routes>
+    </div>
+  )
+}
---
+++ client/src/components/student/CourseSection.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CourseSection() {
+  return (
+    <div>CourseSection</div>
+  )
+}
